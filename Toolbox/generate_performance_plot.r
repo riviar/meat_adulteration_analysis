765c6a0996b32e057d5a58f8142c8cf97a9542aa
Rafal Kural <silvidanfox@gmail.com>
2015-08-04 22:33:32 +0100

    Modified to work with all data types instead of just accuracy scores

--- Toolbox/generate_performance_plot.r
+++ Toolbox/generate_performance_plot.r
@@ -1,34 +1,44 @@
 ##################################################
 # Generates pdf file with performance plot 
 #
+# INPUTS:
 # data - matrix of data to plot
-# names - vector of names for data
+# plotName - string describing the type of plot
+# dataNames - vector of names describing input data elements
 # file - output file
+#
+# OUTPUTS:
+# Returns nothing
+# Generates pdf file containing performance plot
 #
 # Rafal Kural
 ##################################################
 
-generate_performance_plot <- function(data, names, file) {
+generate_performance_plot <- function(data, plotName, dataNames, file) {
   require(colorRamps)
   
   # generate different colors for every data entry
-  colors <- primary.colors(nrow(data), steps = 5, no.white = TRUE)
+  colors <- primary.colors(nrow(data), steps = 3, no.white = TRUE)
   
   # create pdf file
   pdf(file)
   
-  # draw plot
+  # prepare plot drawing area
   par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-  plot(c(0, iterations), c(0, 100), xlab = "Iterations", 
-       ylab = "Accuracy", main = "Models Performance", type = "n")
-  axis(side=2, at=seq(0, 100, by=10))
+  xAxisPreset <- c(0, ncol(data))
+  yAxisPreset <- c(min(data), max(data))
+  plot(xAxisPreset, yAxisPreset, xlab = "Iterations", 
+       ylab = "Score", main = plotName, type = "n")
+  # add steps to axis
+  #axisStep <- (max(data) - min(data)) / 10
+  #axis(side=2, at=seq(min(data), max(data), by = axisStep))
   
   for(i in 1:nrow(data)) {
-    lines(1:ncol(data), data[i,]*100, col = colors[i])
+    lines(1:ncol(data), data[i,], col = colors[i])
   }
   
   # generate legend for plot
-  legend("topright", inset=c(-0.42,0), names, lty=c(1), col=colors, cex = 0.8)
+  legend("topright", inset=c(-0.42,0), dataNames, lty=c(1), col=colors, cex = 0.8)
   
   # close file
   dev.off()
